ad7d565f8f5b7020c4d7e0bd570097b8d91aacd3
yanmiao <yanmiao@shinemo.com>
2022-03-09 10:59:55 +0800

    feat : 配置 log4js 日志

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const log4js = require('log4js')
+// const logger = require('koa-logger')
+const log4js = require("./utils/log4j")
 
 
 const index = require('./routes')
@@ -16,27 +16,28 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+    enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
-app.use(logger())
+// app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
-  extension: 'pug'
+    extension: 'pug'
 }))
+
+// app.use(() => {
+//     ctx.body = "123"
+// })
 
 // logger
 app.use(async (ctx, next) => {
-  // const start = new Date()
-  // await next()
-  // const ms = new Date() - start
-  // console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-  await next()
-  var log = log4js.getLogger();
-  log.level = "debug";
-  log.debug("Some debug messages");
-
+    // const start = new Date()
+    // await next()
+    // const ms = new Date() - start
+    // console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+    await next()
+    log4js.info(ctx.stack)
 })
 
 // routes
@@ -45,7 +46,8 @@ app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+    // console.error('server error', err, ctx)
+    log4js.error(`${err.stack}`)
 });
 
 module.exports = app
