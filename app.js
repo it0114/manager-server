e27e4563290ed65cce39988ccbc86758c0ebfe1a
yanmiao <yanmiao@shinemo.com>
2022-04-04 22:55:10 +0800

    fix : 更改配置文件

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const Koa = require('koa')
 const app = new Koa()
-const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 // const logger = require('koa-logger')
 const log4js = require("./utils/log4j")
-// const cors = require('koa2-cors')
+const cors = require('koa2-cors')
 const router = require('koa-router')()
 
 // 路由
 const users = require('./routes/users')
+// token
+const jwt = require('jsonwebtoken')
 
-//
-// // 允许跨域
-// app.use(
-//     cors({
-//         origin: function (ctx) { //设置允许来自指定域名请求
-//             if (ctx.url === '/test') {
-//                 return '*'; // 允许来自所有域名请求
-//             }
-//             return 'http://localhost:8080'; //只允许http://localhost:8080这个域名的请求
-//         },
-//         maxAge: 5, //指定本次预检请求的有效期，单位为秒。
-//         credentials: true, //是否允许发送Cookie
-//         allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法'
-//         allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
-//         exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
-//     })
-// );
+// 允许跨域
+app.use(
+    cors({
+        origin: function (ctx) { //设置允许来自指定域名请求
+            if (ctx.url === '/test') {
+                return '*'; // 允许来自所有域名请求
+            }
+            return 'http://localhost:8080'; //只允许http://localhost:8080这个域名的请求
+        },
+        maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+        credentials: true, //是否允许发送Cookie
+        allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法'
+        allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
+        exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
+    })
+);
 
 // error handler
 onerror(app)
@@ -36,17 +36,14 @@ onerror(app)
 // 数据库
 require("./config/db")
 
-// middlewares
+// middlewares 中间件
 app.use(bodyparser({
     enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
-// app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
 
-app.use(views(__dirname + '/views', {
-    extension: 'pug'
-}))
+// 用于返回静态文件
+app.use(require('koa-static')(__dirname + '/public'))
 
 // logger
 app.use(async (ctx, next) => {
@@ -68,5 +65,6 @@ app.on('error', (err, ctx) => {
     // console.error('server error', err, ctx)
     log4js.error(`${err.stack}`)
 });
+
 
 module.exports = app
--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 /*
 * 数据库
+* source .bash_profile
 * mac启动命令 : mongod --config /usr/local/mongodb/mongo/conf/mongo.conf
 * */
 const mongoose = require('mongoose')
@@ -7,18 +8,18 @@ const config = require("./index")
 const log4js = require("../utils/log4j")
 
 // 连接数据库
-mongoose.connect(config.URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+mongoose.connect(config.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 
 const db = mongoose.connection
 
-db.on('error',()=>{
+db.on('error', () => {
     log4js.error('***数据库连接失败***')
 })
 
-db.on('open',()=>{
+db.on('open', () => {
     log4js.info('***数据库连接成功***')
 })
 
